9e07fa3fb0b23cae3bd9d5103cfd70b2056a0119
Ian Knighton <46321154+IanKnighton@users.noreply.github.com>
2022-08-17 14:47:25 -0600

    Fixed Name and Removed Another File

--- .tf/GitHub/repo_registration_page.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository" "repo_registration_page" {
-  name                 = "RegistrationPage"
-  description          = "This is .Net Core web application to accept and process pre-registration for an event."
-  has_downloads        = true
-  has_issues           = true
-  has_projects         = true
-  has_wiki             = true
-  vulnerability_alerts = true
-}
